3431ee066a33ad2f239c3069a7f078d4c702ff57
Lunnatica <paulalopezantelo@gmail.com>
2022-02-13 23:27:06 +0000

    Update tests

--- components/ResultsHeader/__tests__/ResultsHeader.spec.tsx
+++ components/ResultsHeader/__tests__/ResultsHeader.spec.tsx
@@ -6,25 +6,27 @@ describe('<ResultsHeader />', () => {
 
     it('should execute the changeTab function when the button is clicked', () => {
         render(<ResultsHeader isInStarredTab={false} changeTab={changeTab} />);
-        fireEvent.click(screen.getByText('Show starred items'));
+        fireEvent.click(screen.getByText('Show starred repos'));
         expect(changeTab).toHaveBeenCalledTimes(1);
     });
 
     describe('when isInStarredTab is true', () => {
-        it('should render the correct button text', () => {
+        it('should render the correct buttons text', () => {
             render(
                 <ResultsHeader isInStarredTab={true} changeTab={changeTab} />
             );
-            expect(screen.getByText('Hide starred items'));
+            expect(screen.getByText('Show all repos'));
+            expect(screen.getByText('Starred repos'));
         });
     });
 
     describe('when isInStarredTab is false', () => {
-        it('should render the correct button text', () => {
+        it('should render the correct buttons text', () => {
             render(
                 <ResultsHeader isInStarredTab={false} changeTab={changeTab} />
             );
-            expect(screen.getByText('Show starred items'));
+            expect(screen.getByText('All repos'));
+            expect(screen.getByText('Show starred repos'));
         });
     });
 });
