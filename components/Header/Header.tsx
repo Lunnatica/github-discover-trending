0ca258a50672a9b1eedc787808779a9db989ccdc
Lunnatica <paulalopezantelo@gmail.com>
2022-02-13 16:30:47 +0000

    Style Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image';
 import { StyledHeader } from './StyledHeader';
 
 const Header = () => (
     <StyledHeader>
+        <Image src="/logo.svg" alt="" width={80} height={80} />
         <h1>Github Trending Discovery</h1>
     </StyledHeader>
 );
--- components/Header/StyledHeader.tsx
+++ components/Header/StyledHeader.tsx
@@ -4,8 +4,8 @@ const StyledHeader = styled.header`
     height: 5rem;
     display: flex;
     align-items: center;
-    padding: 0 1rem;
-    justify-content: space-between;
+    padding: 0.5rem 2rem;
+    justify-content: start;
 `;
 
 export { StyledHeader };
