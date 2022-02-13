b6001cbda22ccb2efbede11a3941adc8c97a529d
Lunnatica <paulalopezantelo@gmail.com>
2022-02-13 14:39:09 +0000

    Add Header

---
+++ components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { StyledHeader } from './StyledHeader';
+
+const Header = () => (
+    <StyledHeader>
+        <h1>Github - Discovery of trending repositories</h1>
+    </StyledHeader>
+);
+
+export { Header };
---
+++ components/Header/StyledHeader.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+    height: 5rem;
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+    justify-content: space-between;
+`;
+
+export { StyledHeader };
