e82ae2972ea94f50b1beaa60de00374bca4290d8
Lunnatica <paulalopezantelo@gmail.com>
2022-02-13 15:01:43 +0000

    Move Metatags to _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,31 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 import { AppProps } from 'next/app';
 
+const MetaTags = () => {
+    const metaUrl = 'http://localhost:3000/';
+    const metaDescription =
+        'Displays a list of the github repositories created in the last 7 days with the most number of stars.';
+    const metaTitle = 'Github Trending Discovery';
+    const metaImage = 'http://localhost:3000/logo.svg';
+
+    return (
+        <Head>
+            <link rel="canonical" href={metaUrl} />
+            <link rel="icon" href="/favicon.ico" />
+            <meta name="Description" content={metaDescription} />
+            <meta property="og:title" content={metaTitle} />
+            <meta property="og:description" content={metaDescription} />
+            <meta property="og:type" content="website" />
+            <meta property="og:image" content={metaImage} />
+            <meta property="og:url" content={metaUrl} />
+            <meta name="twitter:title" content={metaTitle} />
+            <meta name="twitter:description" content={metaDescription} />
+            <meta name="twitter:image" content={metaImage} />
+            <meta name="twitter:card" content="summary" />
+        </Head>
+    );
+};
+
 class MyDocument extends Document {
     static async getInitialProps(ctx: any) {
         const sheet = new ServerStyleSheet();
@@ -32,6 +57,7 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
+                <MetaTags />
                 <body>
                     <Main />
                     <NextScript />
