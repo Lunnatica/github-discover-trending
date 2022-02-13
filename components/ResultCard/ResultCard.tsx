3ab59f09fc9884af7763c1d467b52c773cb697f7
Lunnatica <paulalopezantelo@gmail.com>
2022-02-13 17:14:24 +0000

    Style Card links

--- components/ResultCard/ResultCard.tsx
+++ components/ResultCard/ResultCard.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link';
+
 import { GithubResult } from '../../interfaces/GithubResults';
 import {
+    StyledLink,
     StyledResultCard,
-    StyledResultTitle,
     StyledResultSubdetails,
+    StyledResultTitle,
 } from './StyledResultCard';
 
 const ResultCard: React.FC<GithubResult> = ({
@@ -15,7 +18,11 @@ const ResultCard: React.FC<GithubResult> = ({
 }) => {
     return (
         <StyledResultCard>
-            <StyledResultTitle>{full_name}</StyledResultTitle>
+            <StyledResultTitle>
+                <Link href={url} passHref>
+                    <StyledLink target="_blank">{full_name}</StyledLink>
+                </Link>
+            </StyledResultTitle>
             <StyledResultSubdetails>{description}</StyledResultSubdetails>
             <StyledResultSubdetails>{stargazers_count}</StyledResultSubdetails>
             <StyledResultSubdetails>{url}</StyledResultSubdetails>
--- components/ResultCard/StyledResultCard.tsx
+++ components/ResultCard/StyledResultCard.tsx
@@ -5,6 +5,7 @@ const StyledResultCard = styled.article`
     background-color: lightgrey;
     flex-basis: 80%;
     padding: 1rem;
+    border-bottom: 1px solid #a4a2a2;
 `;
 
 const StyledResultSubdetails = styled.p`
@@ -18,4 +19,23 @@ const StyledResultTitle = styled(StyledResultSubdetails)`
     font-size: x-large;
 `;
 
-export { StyledResultCard, StyledResultSubdetails, StyledResultTitle };
+const linkColor = '#1F52EA'; // A11Y: If color is changed, check https://webaim.org/resources/linkcontrastchecker/
+
+const StyledLink = styled.a`
+    color: ${linkColor};
+    text-decoration: none;
+    font-weight: 600;
+
+    :hover,
+    :active {
+        box-shadow: 0 0.1rem 0 ${linkColor};
+        cursor: pointer;
+    }
+`;
+
+export {
+    StyledResultCard,
+    StyledResultSubdetails,
+    StyledResultTitle,
+    StyledLink,
+};
