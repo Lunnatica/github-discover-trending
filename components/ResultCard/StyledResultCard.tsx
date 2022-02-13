99e81855d7faf815a78dd2f9aab7f25e18271b6a
Lunnatica <paulalopezantelo@gmail.com>
2022-02-13 17:44:51 +0000

    Move colours definition into their own file

--- components/ResultCard/StyledResultCard.tsx
+++ components/ResultCard/StyledResultCard.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import { breakpoints } from '../../styles/breakpoints';
+import { colors } from '../../styles/colors';
 
 const StyledResultCard = styled.article`
-    background-color: lightgrey;
+    background-color: ${colors.background};
     flex-basis: 80%;
     padding: 1rem;
-    border-bottom: 1px solid #a4a2a2;
+    border-bottom: 1px solid ${colors.border};
 `;
 
 const StyledResultSubdetails = styled.p`
@@ -19,16 +20,14 @@ const StyledResultTitle = styled(StyledResultSubdetails)`
     font-size: x-large;
 `;
 
-const linkColor = '#1F52EA'; // A11Y: If color is changed, check https://webaim.org/resources/linkcontrastchecker/
-
 const StyledLink = styled.a`
-    color: ${linkColor};
+    color: ${colors.link};
     text-decoration: none;
     font-weight: 600;
 
     :hover,
     :active {
-        box-shadow: 0 0.1rem 0 ${linkColor};
+        box-shadow: 0 0.1rem 0 ${colors.link};
         cursor: pointer;
     }
 `;
---
+++ styles/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+    border: '#a4a2a2',
+    link: '#1F52EA', // A11Y: If color is changed, check https://webaim.org/resources/linkcontrastchecker/
+    background: 'lightgrey',
+};
